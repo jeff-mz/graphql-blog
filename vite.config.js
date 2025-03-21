2137b4c013a4a6b10cab857192b50d3371e479ae
jafar mirzapoor <jafarmirzapoor11@gmail.com>
2025-03-21 20:16:13 +0330

    added netlify config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+// Fix for ES Modules
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
